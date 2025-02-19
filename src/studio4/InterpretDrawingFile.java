743110e7e81bb70bc47430b324a4fd2d17d26a8a
liuyi <liuyi@DESKTOP-IBQSS4Q.dhcp.wustl.edu>
2025-02-19 15:34:02 -0600

    1

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,19 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree  = in.nextDouble();
+		double parameterFour  = in.nextDouble();
+		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		
 	}
 }
